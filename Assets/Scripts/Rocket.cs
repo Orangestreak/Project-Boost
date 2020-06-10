b639e2755e221e1b9c4428923dc382b2306b2be5
RJones0421 <rjcj0421@gmail.com>
2020-06-10 18:52:31 -0400

    Added Effects and Levels
    Added effects to the game as well as some extra levels to help with testing some functionality. Levels to be changed.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -11,6 +9,18 @@ public class Rocket : MonoBehaviour
 
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip complete;
+    [SerializeField] AudioClip death;
+
+    [SerializeField] ParticleSystem pMainEngine;
+    [SerializeField] ParticleSystem pComplete;
+    [SerializeField] ParticleSystem pDeath;
+
+    bool isTransitioning = false;
+
+    bool collisionsDisabled = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,47 +32,111 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (!isTransitioning)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+
+        if (Debug.isDebugBuild)
+        {
+            RespondToDebugKeys();
+        }
+    }
+
+    private void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        } else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionsDisabled = !collisionsDisabled;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        switch (collision.gameObject.tag)
+        if (isTransitioning || collisionsDisabled)
         {
-            case "Friendly":
-                print("Friendly");
-                break;
+            return;
+        }
 
-            default:
-                print("Deadly");
-                break;
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+
+        if (collision.relativeVelocity.magnitude > 10)
+        {
+            isTransitioning = true;
+            Invoke("ResetLevel", 2f);
+            StopEffects();
+            audioSource.PlayOneShot(death); //TODO find better death noise
+            pDeath.Play();
+        }
+        else
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "Friendly":
+                    print("Friendly");
+                    break;
+
+                case "Finish":
+                    isTransitioning = true;
+                    Invoke("LoadNextLevel", 1.5f);
+                    StopEffects();
+                    audioSource.PlayOneShot(complete);
+                    pComplete.Play();
+                    break;
+
+                default:
+                    isTransitioning = true;
+                    Invoke("ResetLevel", 2f);
+                    StopEffects();
+                    audioSource.PlayOneShot(death); //TODO find better death noise
+                    pDeath.Play();
+                    break;
+            }
         }
     }
 
-    private void Thrust()
+    private void LoadNextLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+        if (currentScene == SceneManager.sceneCountInBuildSettings)
+        {
+            nextScene = 0;
+        }
+        SceneManager.LoadScene(nextScene);
+    }
+
+    private void ResetLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            //start playing audio if not playing
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
         //stop audio if not holding space
         else
         {
-            audioSource.Stop();
+            StopEffects();
         }
     }
 
-    private void Rotate()
+    private void RespondToRotateInput()
     {
 
         //freeze physics when rotating
-        rigidBody.freezeRotation = true;
+        rigidBody.angularVelocity = Vector3.zero;
 
         float rotationThisFrame = rcsThrust * Time.deltaTime;
 
@@ -74,8 +148,23 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(Vector3.back * rotationThisFrame);
         }
+    }
 
-        //resume physics
-        rigidBody.freezeRotation = false;
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        //start playing audio if not playing
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        pMainEngine.Play(); //TODO improve particles
+        //currently showing where they would be not what it should be
+    }
+
+    private void StopEffects()
+    {
+        audioSource.Stop();
+        pMainEngine.Stop();
     }
 }
